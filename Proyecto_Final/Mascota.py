d7ae1d8a2d449296a47466a5429f6abbc27c380a
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-04-15 23:49:00 -0500

    Codigo base class Perro

--- Proyecto_Final/Mascota.py
+++ Proyecto_Final/Mascota.py
@@ -4,10 +4,34 @@ class Adopcion:
         self.edad =edadm_mascota
         self.raza =raza_mascota
         self.__salud =salud_mascota
+        self.sonido =sonido_emitido
   
+
     def mostrar_informacion(self)-> str:
        return f"Nombre de la mascota {self.nombre}, la edad que tiene te la mostraremos en meses {self.edad} meses,\n la raza a la cual pertenece es {self.raza} y su estado de salud es {self.salud}"
    
+class Perro:
+   def __init__(self, energia:str, sonido:str)-> None:
+       self.energia =nivel_energia
+       self.sonido =sonido_emitido
+    
+    def mostrar_informacion(self)-> str:
+        return f"El nivel de energia de la mascota es {self.energia} y el nivel de sonido qu emite la mascota es {self.sonido}"
+  
+
+
+  
+
+  
+
+  
+
+  
+
+  
+
+  
+
   
 # Ejemplo de las respuestas que recibira el codigo
       
