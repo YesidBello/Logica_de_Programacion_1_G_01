a45963440ed029531896bc31d4e1773b38d3dd8d
Michelle Salgado <msalgado77@unisalle.edu.co>
2026-04-15 23:03:30 -0500

    Codigo base

--- Proyecto_Final/Mascota.py
+++ Proyecto_Final/Mascota.py
@@ -1,5 +1,18 @@
 class Adopcion:
-
+    def __init__(self, nombre:str, edad:int, raza:str, salud:str)-> None:
+        self.nombre =nombre_mascota 
+        self.edad =edadm_mascota
+        self.raza =raza_mascota
+        self.salud =salud_mascota
+  
+    def mostrar_informacion(self)-> str:
+       return f"Nombre de la mascota{self.nombre}, la edad que tiene te la mostraremos en meses{self.edad}\n, la raza a la cual pertenece es{self.raza} y su estado de salud es{self.salud}"
+       
+  
+  
+  
+  
+    
 
   
 
